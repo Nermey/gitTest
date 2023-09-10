2eac5f6d7814979d4a63f291aeb258aa872aa152
Nermey <elik19898@gmail.com>
2023-09-10 23:09:01 +0300

    do conflict

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("rebuild the world!");
         System.out.println("commit3");
-        test t = new test();
+        test1 t = new test1();
         t.a = 12;
+        t.s = "qux";
         System.out.println(t.a);
+        System.out.println(t.s);
     }
 }
--- src/test1.java
+++ src/test1.java
@@ -1,4 +1,4 @@
-public class test {
+public class test1 {
     public int a;
     public String s;
 }
