822703070297a870b8d9ce76032d10108da631cc
Nermey <elik19898@gmail.com>
2023-09-10 22:44:40 +0300

    test conflict

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("rebuild the world!");
         System.out.println("commit3");
+        test t = new test();
+        t.a = 12;
+        System.out.println(t.a);
     }
 }
