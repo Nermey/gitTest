822c2e8735c7f9372129dffc5155e98b5997235d
Nermey <elik19898@gmail.com>
2023-09-10 23:18:28 +0300

    add print

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("rebuild the world!");
         System.out.println("commit3");
-        test1 t = new test1();
-        t.a = 12;
-        t.s = "qux";
-        System.out.println(t.a);
-        System.out.println(t.s);
+        print("ok");
+
     }
+
+    public static void print(String s) {
+        System.out.println(s);
+    }
+
 }
--- src/test1.java
+++
@@ -1,3 +0,0 @@
-public class test1 {
-    public int a;
-}
